9ec444633a031197b9e0f39446ce69da0f63efd1
fikrifiko <fikrifiko@users.noreply.github.com>
2025-01-26 03:51:39 +0100

    1:Ajout des endpoitns pour client 2: changement du endpoint reservation client vers reservation/index

---
+++ Migrations/20250126004546_TestMOhanad5.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Table_Reservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMOhanad5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+    name: "CanvasModels",
+    columns: table => new
+    {
+        Id = table.Column<int>(type: "int", nullable: false)
+            .Annotation("SqlServer:Identity", "1, 1"),
+        Width = table.Column<int>(type: "int", nullable: false),
+        Height = table.Column<int>(type: "int", nullable: false)
+    },
+    constraints: table =>
+    {
+        table.PrimaryKey("PK_CanvasModels", x => x.Id);
+    });
+
+            migrationBuilder.CreateTable(
+                name: "ClientModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ClientEmail = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ClientPhone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TableId = table.Column<int>(type: "int", nullable: false),
+                    ReservationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReservationHoure = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientPhone = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    X = table.Column<int>(type: "int", nullable: false),
+                    Y = table.Column<int>(type: "int", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Seats = table.Column<int>(type: "int", nullable: false),
+                    Rotated = table.Column<bool>(type: "bit", nullable: false),
+                    Reserved = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tables", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Validator/ClientModelValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Table_Reservation.Models;
+
+namespace Table_Reservation.Validator
+{
+    public class ClientModelValidator: AbstractValidator<ClientModel>
+    {
+        public ClientModelValidator()
+        {
+            //Regle pour dire qu'il faut que le nom client ne soit pas empty
+            RuleFor(client => client.ClientName)
+                .NotEmpty().WithMessage("Client Name Is Required");
+
+            //Regle pour dire qu'il faut que le email client est required et que le format doit etre une adresse mail. càd avec le @ et le .be ou .com etc.
+            RuleFor(client => client.ClientEmail)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            //Regle pour dire que le numéro de telepgone doit au moins contenir 9 charactere (possivilté d'ajouter d'autres regles, mais pour l'instant on fait comme ca.
+            RuleFor(client => client.ClientPhone).NotEmpty()
+                .MinimumLength(9).WithMessage("Please enter a valid phone number");
+        }
+    }
+}
